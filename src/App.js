e7413870d210d934452c5f8bab19d71b2c33a8f7
Durga_prasad <prasadprasaddurga879@gmail.com>
2022-09-28 07:11:21 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,213 @@
+import {Component} from 'react'
+import {v4} from 'uuid'
+import PasswordItem from './components/PasswordItem'
+import './App.css'
+
+class App extends Component {
+  state = {
+    passwordRecords: [],
+    inputUrl: '',
+    inputName: '',
+    inputPassword: '',
+    searchInput: '',
+    showPassword: false,
+  }
+
+  addPasswordRecord = event => {
+    event.preventDefault()
+    const {inputName, inputUrl, inputPassword} = this.state
+    const newItem = {
+      id: v4(),
+      url: inputUrl,
+      name: inputName,
+      password: inputPassword,
+    }
+    this.setState(prevState => ({
+      passwordRecords: [...prevState.passwordRecords, newItem],
+    }))
+  }
+
+  onInputUrlChange = event => {
+    this.setState({inputUrl: event.target.value})
+  }
+
+  onInputNameChange = event => {
+    this.setState({inputPassword: event.target.value})
+  }
+
+  onInputPasswordChange = event => {
+    this.setState({inputName: event.target.value})
+  }
+
+  onSearchChange = event => {
+    this.setState({searchInput: event.target.value})
+  }
+
+  getSearchRecords = () => {
+    const {passwordRecords, searchInput} = this.state
+    return passwordRecords.filter(each =>
+      each.url.toLowerCase().includes(searchInput.toLowerCase()),
+    )
+  }
+
+  deletePasswordRecord = id => {
+    const {passwordRecords} = this.state
+    const updatedRecords = passwordRecords.filter(each => each.id !== id)
+    this.setState({passwordRecords: updatedRecords})
+  }
+
+  onCheckChange = () => {
+    this.setState(prevState => ({
+      showPassword: !prevState.showPassword,
+    }))
+  }
+
+  render() {
+    const searchResults = this.getSearchRecords()
+    const {showPassword} = this.state
+    return (
+      <div className="app-container">
+        <div className="responsive-container">
+          <div className="app-header">
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/password-manager-logo-img.png"
+              alt="app logo"
+              className="app-logo"
+            />
+          </div>
+          <div className="card-container manager-container">
+            <img
+              src=" https://assets.ccbp.in/frontend/react-js/password-manager-sm-img.png"
+              alt="password manager"
+              className="manager-image"
+            />
+            <div className="card form-container">
+              <form
+                className="card-responsive"
+                onSubmit={this.addPasswordRecord}
+              >
+                <h1 className="form-heading">Add New Password</h1>
+                <div className="input-container">
+                  <div className="icon-container">
+                    <img
+                      className="input-icon"
+                      src="https://assets.ccbp.in/frontend/react-js/password-manager-website-img.png"
+                      alt="website"
+                    />
+                  </div>
+
+                  <input
+                    className="input"
+                    type="text"
+                    placeholder="Enter Website"
+                    onChange={this.onInputUrlChange}
+                  />
+                </div>
+                <div className="input-container">
+                  <div className="icon-container">
+                    <img
+                      className="input-icon"
+                      src="https://assets.ccbp.in/frontend/react-js/password-manager-username-img.png"
+                      alt="username"
+                    />
+                  </div>
+                  <input
+                    className="input"
+                    type="text"
+                    placeholder="Enter Username"
+                    onChange={this.onInputNameChange}
+                  />
+                </div>
+                <div className="input-container">
+                  <div className="icon-container">
+                    <img
+                      className="input-icon"
+                      src="https://assets.ccbp.in/frontend/react-js/password-manager-password-img.png"
+                      alt="password"
+                    />
+                  </div>
+                  <input
+                    className="input"
+                    type="password"
+                    placeholder="Enter Password"
+                    onChange={this.onInputPasswordChange}
+                  />
+                </div>
+                <div className="btn-container">
+                  <button
+                    className="add-btn"
+                    type="submit"
+                    onClick={this.addPasswordRecord}
+                  >
+                    Add
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+
+          <div className="card-container">
+            <div className="card-responsive no-password-container">
+              <div className="passwords-header">
+                <h1 className="passwords-header-title">
+                  Your Passwords
+                  <p className="results-count"> {searchResults.length}</p>
+                </h1>
+                <div className="search-container">
+                  <div className="search-icon-container">
+                    <img
+                      src="https://assets.ccbp.in/frontend/react-js/password-manager-search-img.png"
+                      alt="search"
+                      className="search-icon"
+                    />
+                  </div>
+                  <input
+                    className="search-input"
+                    type="search"
+                    placeholder="Search"
+                    onChange={this.onSearchChange}
+                  />
+                </div>
+              </div>
+              <hr className="hr-line" />
+              <div className="checkbox-container">
+                <input
+                  type="checkbox"
+                  className="checkbox-input"
+                  id="checkbox"
+                  onChange={this.onCheckChange}
+                />
+                <label htmlFor="checkbox" className="checkbox-label">
+                  Show Passwords
+                </label>
+              </div>
+              {searchResults.length !== 0 ? (
+                <ul className="passwords-list-container">
+                  {searchResults.map(eachRecord => (
+                    <PasswordItem
+                      key={eachRecord.id}
+                      record={eachRecord}
+                      deletePasswordRecord={this.deletePasswordRecord}
+                      showPassword={showPassword}
+                    />
+                  ))}
+                </ul>
+              ) : (
+                <div className="no-password-container">
+                  <img
+                    src="https://assets.ccbp.in/frontend/react-js/no-passwords-img.png"
+                    alt="no passwords"
+                    className="no-passwords-image"
+                  />
+                  <p className="no-passwords-title">No Passwords</p>
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App
